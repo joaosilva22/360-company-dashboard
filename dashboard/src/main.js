d13c8d75712a5a3e4c7a04a4bd992ce7bd6881c2
Lázaro Costa <lazaroosta@hotmail.com>
2017-11-03 15:38:56 +0000

    new pages

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -1,15 +1,31 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Main from '@/components/Main';
+import Sales from '@/components/Sales';
+import Overview from '@/components/Overview';
+import Financial from '@/components/Financial';
+import SupplyChain from '@/components/SupplyChain';
 import Vue from 'vue';
+import VueRouter from 'vue-router';
 import App from './App';
-import router from './router';
 
-Vue.config.productionTip = false;
+Vue.use(VueRouter);
 
+const routes = [
+  { path: '/sales', component: Sales },
+  { path: '/overview', component: Overview },
+  { path: '/financial', component: Financial },
+  { path: '/supplyChain', component: SupplyChain },
+  { path: '/', component: Main },
+];
+
+const router = new VueRouter({
+  routes,
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
-  components: { App },
+  render: h => h(App),
 });
+
