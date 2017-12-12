b78cf5f993b2b88ef1da18445a570ceffedb7284
DiogoDuque <diogoduque25@gmail.com>
2017-12-12 00:11:17 +0000

    added income and expenses backend code

--- dashboard/src/services/Account.js
+++ dashboard/src/services/Account.js
@@ -3,10 +3,13 @@ import axios from 'axios';
 export default {
   receivable() {
     return this.$axios.get('accountReceivable');
-    console.log(this.currentAssets(2016));
   },
 
   currentRatio(int fiscalYear) {
   	return this.$axios.get(`AuditFile/CurrentRatio?FiscalYear=${fiscalYear}`);
-  }
+  },
+
+  incomeAndExpenses(int fiscalYear) {
+  	return this.$axios.get(`AuditFile/CurrentRatio?FiscalYear=${fiscalYear}`);
+  },
 };
